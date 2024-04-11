61241610d9985a9fd94226782a97f8b5b2e5bfe8
zajnetdinov <dzajnetdinov285@gmail.com>
2024-04-11 19:23:03 +0500

    vbi;

--- asyncs.py
+++ asyncs.py
@@ -1,10 +1,12 @@
 import os
 
+import requests
 from dotenv import load_dotenv
 from telegram.ext import ConversationHandler
 
 load_dotenv()
 apikey = os.getenv('apikey')
+
 
 async def first_response(update, context):
     await update.message.reply_text(
@@ -18,15 +20,24 @@ async def first_response(update, context):
 async def get_attractions(update, context):
 
     locality = update.message.text
+    first_request = f'https://catalog.api.2gis.com/2.0/region/search?q=Миасс&key={apikey}'
+    first_response = requests.get(first_request).json()
+    city_id = first_response['result']['items'][0]['id']
+    second_request = (f'https://catalog.api.2gis.com/2.0/catalog/rubric/search?region_id='
+                       f'{city_id}&q=достопримечательности&key={apikey}')
+    second_response = requests.get(second_request).json()
+    attraction_id = [
+
+    ]
     await update.message.reply_text(
         f"Вот некоторые достопримечательности в городе {locality}")
     return 2
 
 
-async def second_response(update, context):
-    await update.message.reply_text(
-        f"Что бы вы хотели узнать о {}"
-    )
+# async def second_response(update, context):
+#     await update.message.reply_text(
+#         f"Что бы вы хотели узнать о {}"
+#     )
 
 
 async def stop(update, context):
--- test.py
+++ test.py
@@ -1,10 +1,15 @@
 import json
 import requests
 
-
-def get_city():
-    request = 'https://catalog.api.2gis.com/2.0/region/search?q=Миасс&key=3919965f-bf13-41f1-8905-c1b49996f645'
-    response = requests.get(request).json()
-    with open('test.json', 'w') as tj:
-        json.dump(response, tj)
-
+# request = 'https://catalog.api.2gis.com/2.0/region/search?q=Миасс&key=3919965f-bf13-41f1-8905-c1b49996f645'
+# response = requests.get(request).json()
+# print(response)
+# city_id = response['result']['items'][0]['id']
+request2 = 'https://catalog.api.2gis.com/2.0/catalog/rubric/search?region_id=87&q=достопримечательности&key=3919965f-bf13-41f1-8905-c1b49996f645'
+response2 = requests.get(request2).json()
+ai = response2['result']['items'][0]['alias']
+print(ai)
+# requests3 = 'https://catalog.api.2gis.com/3.0/items?rubric_id=112670&region_id=87&key=3919965f-bf13-41f1-8905-c1b49996f645'
+# response2 = requests.get(requests3).json()
+# with open('test.json', 'w') as r2:
+#     json.dump(response2, r2, indent=3)
