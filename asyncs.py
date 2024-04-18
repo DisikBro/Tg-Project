43acdbe679c11ecbc3ee09502e3f6f88fd91439d
zajnetdinov <dzajnetdinov285@gmail.com>
2024-04-18 19:34:23 +0500

    предвсе

--- asyncs.py
+++ asyncs.py
@@ -8,35 +8,48 @@ from telegram.ext import ConversationHandler
 load_dotenv()
 apikey = os.getenv('apikey')
 
+attraction_ids = []
+city_ids = []
+
 
 async def start(update, context):
     await update.message.reply_text(
         'Добро пожаловать! Это бот - Mr. Достопримечательный. Я покажу самые интересные '
         'достопримечательности в выбранном вами городе!\n'
-        'Какой город вас интересует?'
+        'Какой город вас интересует?', reply_markup=ReplyKeyboardRemove()
     )
     return 1
 
 
 async def get_attractions(update, context):
-
     locality = update.message.text
-    # first_request = f'https://catalog.api.2gis.com/2.0/region/search?q={locality}&key={apikey}'
-    # first_response = requests.get(first_request).json()
-    # city_id = first_response['result']['items'][0]['id']
-    # second_request = (f'https://catalog.api.2gis.com/2.0/catalog/rubric/search?region_id={city_id}'
-    #                   f'&q=достопримечательности&key={apikey}')
-    # second_response = requests.get(second_request).json()
-
-    attraction_id = []
+    first_request = f'https://catalog.api.2gis.com/2.0/region/search?q={locality}&key={apikey}'
+    first_response = requests.get(first_request).json()
+    city_id = first_response['result']['items'][0]['id']
+    city_ids.append(city_id)
+    second_request = (f'https://catalog.api.2gis.com/2.0/catalog/rubric/search?region_id={city_id}'
+                        f'&q=достопримечательности&key={apikey}')
+    second_response = requests.get(second_request).json()
+    for i in range(4):
+        attraction_ids.append(second_response['result']['items'][i]['id'])
+    await update.message.reply_text(attraction_ids)
 
     await update.message.reply_text(
         "Какие достопримечательности вас интересуют?", reply_markup=markup)
-    return 2
 
 
 async def observation_platforms(update, context):
-    await update.message.reply_text('observation_platforms')
+    rubric_items = []
+    third_requests = (f'https://catalog.api.2gis.com/3.0/items?rubric_id='
+                      f'{attraction_ids[0]}&region_id={city_ids[0]}&key={apikey}')
+    third_response = requests.get(third_requests).json()
+    for i in range(len(third_response['result']['items'])):
+        for j in third_response['result']['items'][i]:
+            if j == 'building_name':
+                rubric_items.append(third_response['result']['items'][i][j])
+    await update.message.reply_text('Вот список смотровых площадок')
+    for i in rubric_items:
+        await update.message.reply_text(i)
 
 
 async def fountains(update, context):
@@ -63,6 +76,6 @@ async def close_keyboard(update, context):
     )
 
 
-reply_keyboard = [['/1', '/2'],
-                  ['/3', '/4']]
+reply_keyboard = [['/observation_platforms', '/fountains'],
+                  ['/interesting_buildings', '/natural_attractions']]
 markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
--- test.py
+++ test.py
@@ -1,15 +1,41 @@
 import json
 import requests
 
-a = []
+ids = []
 
-second_request = ('https://catalog.api.2gis.com/2.0/catalog/rubric/search?region_id=32'
-                '&q=достопримечательности&key=3919965f-bf13-41f1-8905-c1b49996f645')
-second_response = requests.get(second_request).json()
-print(second_response)
-for i in range(4):
-    a.append(second_response['result']['items'][i]['caption'])
-print(a)
+# locality = input()
+# first_request = f'https://catalog.api.2gis.com/2.0/region/search?q={locality}&key=3919965f-bf13-41f1-8905-c1b49996f645'
+# first_response = requests.get(first_request).json()
+# print(first_response)
+# if first_response['meta']['error']['message'] == 'Results not found':
+#     print("Введите действительный город!")
+
+third_requests = 'https://catalog.api.2gis.com/3.0/items?rubric_id=112670&region_id=77&key=3919965f-bf13-41f1-8905-c1b49996f645'
+third_response = requests.get(third_requests).json()
+for i in range(len(third_response['result']['items'])):
+    for j in third_response['result']['items'][i]:
+        if j == 'building_name':
+            ids.append(third_response['result']['items'][i][j])
+print(ids)
+# for i in third_response['result']['items']:
+#     if i == 'building_name':
+#         ids.append(third_response['result']['items'][i])
+# print(ids)
+
+#
+# with open('test2.json', 'w') as t:
+#     json.dump(third_response, t, indent=3)
+
+# print(second_response)
+
+
+# second_request = ('https://catalog.api.2gis.com/2.0/catalog/rubric/search?region_id=32'
+#                 '&q=достопримечательности&key=3919965f-bf13-41f1-8905-c1b49996f645')
+# second_response = requests.get(second_request).json()
+# print(second_response)
+# for i in range(4):
+#     a.append(second_response['result']['items'][i]['caption'])
+# print(a)
 
 # requests3 = 'https://catalog.api.2gis.com/3.0/items?rubric_id=112670&region_id=87&key=3919965f-bf13-41f1-8905-c1b49996f645'
 # response2 = requests.get(requests3).json()
